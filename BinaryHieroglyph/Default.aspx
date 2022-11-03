3db8f77885e5f6d45da439ac55673d4cc244f8b2
James Kirk <KIRKSTER_515@hotmail.com>
2022-11-03 05:17:46 +0000

    Add project files.

---
+++ BinaryHieroglyph/Default.aspx
@@ -0,0 +1,37 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="BinaryHieroglyph.Default" %>
+
+<!DOCTYPE html>
+<link rel="stylesheet" href="Styles/style.css" media="screen" />
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+           <asp:TextBox runat="server" ID="txtNumber"></asp:TextBox>
+            <asp:Button runat="server" ID="btnCalculate" Text="Calculate" OnClick="btnCalculate_Click"  /><br /> <br />
+            <div style="font-size:0px;">
+                <div>
+                    <asp:Label ID="lblLeft1" Width="10px" Text="&nbsp" runat="server" CssClass="StyleLeftOn"></asp:Label>
+                    <asp:Label ID="lblRight1" Width="10px" Text="&nbsp" runat="server" CssClass="StyleRightOn"></asp:Label>
+                </div>
+                <div>
+                    <asp:Label ID="lblLeft2" Width="10px" Text="&nbsp" runat="server" CssClass="StyleLeftOn"></asp:Label>
+                    <asp:Label ID="lblRight2" Width="10px" Text="&nbsp" runat="server" CssClass="StyleRightOn"></asp:Label>
+                </div>
+                <div>
+                    <asp:Label ID="lblLeft3" Width="10px" Text="&nbsp" runat="server"></asp:Label>
+                    <asp:Label ID="lblRight3" Width="10px" Text="&nbsp" runat="server"></asp:Label>
+                </div>
+                <div>
+                    <asp:Label ID="lblLeft4" Width="10px" Text="&nbsp" runat="server"></asp:Label>
+                    <asp:Label ID="lblRight4" Width="10px" Text="&nbsp" runat="server"></asp:Label>
+                </div>
+                
+            </div>X
+
+        </div>
+    </form>
+</body>
+</html>
---
+++ BinaryHieroglyph/Default.aspx.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace BinaryHieroglyph
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnCalculate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int number = Convert.ToInt32(txtNumber.Text);
+
+                int maxPower = 8;
+                CheckForLabelBorder(lblRight4, ref number, LabelSide.Right, --maxPower);
+                CheckForLabelBorder(lblLeft4, ref number, LabelSide.Left, --maxPower);
+                CheckForLabelBorder(lblRight3, ref number, LabelSide.Right, --maxPower);
+                CheckForLabelBorder(lblLeft3, ref number, LabelSide.Left, --maxPower);
+                CheckForLabelBorder(lblRight2, ref number, LabelSide.Right, --maxPower);
+                CheckForLabelBorder(lblLeft2, ref number, LabelSide.Left, --maxPower);
+                CheckForLabelBorder(lblRight1, ref number, LabelSide.Right, --maxPower);
+                CheckForLabelBorder(lblLeft1, ref number, LabelSide.Left, --maxPower);
+            }
+            catch { }
+        }
+
+        private enum LabelSide
+        {
+            Left,
+            Right
+        }
+
+        private void CheckForLabelBorder(Label label, ref int number, LabelSide side, int powerToCheckFor)
+        {
+            bool isInPower = IsNumberHigherThanPower(ref number, powerToCheckFor);
+            label.Style.Clear();
+            label.CssClass = "Style" + side.ToString() + (isInPower ? "On" : "Off");
+        }
+
+        private bool IsNumberHigherThanPower(ref int numIn, int powerToCheckFor)
+        {
+            int power = Convert.ToInt32(Math.Pow(2, powerToCheckFor));
+            if (numIn >= power)
+            {
+                numIn -= power;
+                return true;
+            }
+            else
+                return false;
+        }
+    }
+}
